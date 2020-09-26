b0d593570971690f8c96d87cc009a9f14b6ae423
Scott Schaefer <scottschaef@gmail.com>
2020-09-25 20:31:25 -0400

    fix subaccounts and basic google metrics

--- server/routes/adServices.js
+++ server/routes/adServices.js
@@ -1,17 +1,8 @@
 const router = require('express-promise-router')();
-const passport = require('passport');
 
 const AdServicesController = require('../controllers/adServices');
 const AuthController = require('../controllers/auth');
-require('../passport'); // require passport configuration
 
-const passportGoogle = passport.authenticate('google',
-  {
-    session: false,
-    scope: ['https://www.googleapis.com/auth/adwords', 'https://www.googleapis.com/auth/userinfo.email'],
-    accessType: 'offline'
-  }
-);
 // const passportFacebook = passport.authenticate('facebook', { session: false });
 
 // router.route('/secret')
@@ -27,7 +18,7 @@ router.route('/get-google-ad-metrics')
   .get(AuthController.verify, AuthController.verifyRole(['client-admin', 'user']), AdServicesController.getGoogleAdMetrics);
 
 router.route('/get-sub-accounts')
-  .post(AuthController.verify, AuthController.verifyRole(['tenant-admin', 'root']), AdServicesController.getSubAccounts);
+  .get(AuthController.verify, AuthController.verifyRole(['tenant-admin', 'root']), AdServicesController.getSubAccounts);
 
 // router.route('/oauth/facebook')
 //   .get(passportFacebook);
