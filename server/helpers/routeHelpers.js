633069cb8fdf3a8ac288d0467c9d410d600338ac
Scott Schaefer <scottschaef@gmail.com>
2020-11-09 20:40:41 -0500

    fix joi

--- server/helpers/routeHelpers.js
+++ server/helpers/routeHelpers.js
@@ -2,7 +2,7 @@ const Joi = require('joi')
 
 module.exports = {
   validateBody: (schema) => (req, res, next) => {
-    const result = Joi.validate(req.body, schema)
+    const result = schema.validate(req.body)
     if (result.error) {
       return res.status(400).json(result.error)
     }
